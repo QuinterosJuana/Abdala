89d5d2eaa3b5cbbe46d256e9d07699aad0d4a670
QuinterosJuana <128099903+QuinterosJuana@users.noreply.github.com>
2023-03-31 19:49:11 -0300

    Numero Perfecto

---
+++ Numero Perecto/js/function.js
@@ -0,0 +1,12 @@
+function nPerfecto(n){
+
+    let suma=0;
+       
+        for (let j=1; j<n; j++) {
+          if (n % j == 0){
+            suma = suma + j;
+          }
+        }
+       
+        return suma==n;
+}
