fac3bba766f8ef3d43cc67836d470fd49b075f28
QuinterosJuana <128099903+QuinterosJuana@users.noreply.github.com>
2023-04-13 18:31:04 -0300

    bino

---
+++ Bino/js/function.js
@@ -0,0 +1,44 @@
+function ContarMultiplosDe2(numero1){
+    let numero2 = 2, resto = 0;
+        
+        resto = numero1%numero2;
+
+        if (resto == 0){
+        document.write(numero1 + "es multipo de 2");
+    }
+
+
+}
+function ContarMultiplosDe3(numero1){
+    let numero2 = 2, resto = 0;
+        
+        resto = numero1%numero2;
+
+        if (resto == 0){
+        document.write(numero1 + "es multipo de 3");
+    }
+
+
+}
+function ContarMultiplosDe4(numero1){
+    let numero2 = 2, resto = 0;
+        
+        resto = numero1%numero2;
+
+        if (resto == 0){
+        document.write(numero1 + "es multipo de 4");
+    }
+
+
+}
+function ContarMultiplosDe5(numero1){
+    let numero2 = 2, resto = 0;
+        
+        resto = numero1%numero2;
+
+        if (resto == 0){
+        document.write(numero1 + "es multipo de 5");
+    }
+
+
+}
