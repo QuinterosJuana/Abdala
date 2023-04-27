c4e70d58b3b5bbc0b87305032711382fa59997f0
QuinterosJuana <128099903+QuinterosJuana@users.noreply.github.com>
2023-04-27 17:00:05 -0300

    Mensaje Secreto

---
+++ Mensaje Secreto/js/function.js
@@ -0,0 +1,24 @@
+function MensajeSecreto(x){
+
+    let frase = '', copia = 1, inicio = 0, fin = 0;
+
+    for(let i = 0; i < x.length; i++){
+
+        
+        if (x[i] == "("){
+            copia = 0; 
+            inicio = i;        
+        }if (copia == 1){
+            frase = frase + x[i];
+        }if(x[i] == ")"){
+            copia = 1;
+            fin = i;
+
+            for (let j = fin-1; j > inicio; j--){
+                frase = frase + x[j];
+            }
+        }
+        
+    }
+    return frase;
+}
