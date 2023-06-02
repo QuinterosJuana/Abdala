59b4dbb958a012016df5b71b8875e9ca856e53d2
QuinterosJuana <128099903+QuinterosJuana@users.noreply.github.com>
2023-06-02 17:59:04 -0300

    Houston

---
+++ Houston/js/function.js
@@ -0,0 +1,13 @@
+function Houston(array1, array2, num) {
+    
+    let houston = [];
+    
+    array1.sort(), array2.sort();
+
+    for (let i = 0; i < num; i++) {
+        total[i] = array1[i] * array2[i];
+    }
+    return houston;
+
+    
+}
