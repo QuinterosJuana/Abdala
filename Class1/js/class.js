4c3d76e22aed3080aae458e0f19eeab7fb22865b
QuinterosJuana <128099903+QuinterosJuana@users.noreply.github.com>
2023-06-30 14:09:52 -0300

    Class1

---
+++ Class1/js/class.js
@@ -0,0 +1,11 @@
+class CRectangulo{
+    
+    constructor(longitud, altura){
+
+        this.longitud = longitud;
+        this.altura = altura;
+    }
+    CalcularArea(){
+        return this.longitud * this.altura;
+    }
+}
