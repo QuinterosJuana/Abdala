e689fa8844f5ec5254fae21d7841d11f0358261c
QuinterosJuana <128099903+QuinterosJuana@users.noreply.github.com>
2023-08-17 02:52:43 -0300

    Repaso

---
+++ Repaso/js/function.js
@@ -0,0 +1,14 @@
+function contarDigitos(numeroIngresado) {
+
+    let count = 0;
+    
+    for (let i = 0; i <= numeroIngresado.length; i++){
+
+        count++;
+
+    }
+
+    return count;
+}
+
+
